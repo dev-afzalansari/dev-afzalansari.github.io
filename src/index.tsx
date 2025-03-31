a62837a47b6395b271db01b4d73d955dd5613ab3
Afzal Ansari <89535738+dev-afzalansari@users.noreply.github.com>
2025-03-31 10:14:42 +0000

    Switch to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./pages/Home";
+import Projects from "./pages/projects/Projects";
+import SimpleJournal from "./pages/projects/simple journal/Simple Journal";
+import SJPrivacyPolicy from "./pages/projects/simple journal/PrivacyPolicy";
 
-import Home from './pages/Home'
-import Projects from './pages/projects/Projects'
-import SimpleJournal from './pages/projects/simple journal/Simple Journal'
-import SJPrivacyPolicy from './pages/projects/simple journal/PrivacyPolicy'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/projects',
-    element: <Projects />
-  },
-  {
-    path: '/projects/simple-journal',
-    element: <SimpleJournal />
-  },
-  {
-    path: '/projects/simple-journal/privacy-policy',
-    element: <SJPrivacyPolicy />
-  }
-])
-
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/simple-journal" element={<SimpleJournal />} />
+        <Route path="/projects/simple-journal/privacy-policy" element={<SJPrivacyPolicy />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
