55fdafa94d2252c45b958413c7b269e46a2f0cae
Afzal <dev.afzalansari@gmail.com>
2025-04-01 20:24:47 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -14,20 +14,21 @@ import SJPrivacyPolicy from "./pages/projects/simple-journal/PrivacyPolicy";
 import BRPrivacyPolicy from "./pages/projects/basket-rush/PrivacyPolicy";
 
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+  
+          <Route path="/projects/simple-journal" element={<SimpleJournal />} />
+          <Route path="/projects/basket-rush" element={<BasketRush />} />
+  
+          <Route path="/projects/simple-journal/privacy-policy" element={<SJPrivacyPolicy />} />
+          <Route path="/projects/basket-rush/privacy-policy" element={<BRPrivacyPolicy />} />
+  
+        </Routes>
+      </HashRouter>
+    </StrictMode>
+  );
 
-        <Route path="/projects/simple-journal" element={<SimpleJournal />} />
-        <Route path="/projects/basket-rush" element={<BasketRush />} />
-
-        <Route path="/projects/simple-journal/privacy-policy" element={<SJPrivacyPolicy />} />
-        <Route path="/projects/basket-rush/privacy-policy" element={<BRPrivacyPolicy />} />
-
-      </Routes>
-    </HashRouter>
-  </StrictMode>
-);
